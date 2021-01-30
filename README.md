6e32c3a76b0a1557f541c93109ccb74ad22af21a
Marcin Zogrodnik <m.zog@icloud.com>
2021-01-30 11:00:11 +0100

    clean up default theme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Custom WordPress theme for S.A.L - MET website.
 
 [sal-met.com](http://sal-met.com)
+
+`npm run css` - compile scss to css
--- functions.php
+++ functions.php
@@ -28,21 +28,21 @@ if ( ! defined( '_S_VERSION' ) ) {
  * Enqueue scripts and styles.
  */
 function sal_met_scripts() {
-	// wp_enqueue_style( 'sal-met-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'defaults-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'sal-met-style', get_template_directory_uri() . '/sal-met.min.css', false, time(), 'all' );
 
 	wp_enqueue_script( 'sal-met-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-	// 	wp_enqueue_script( 'comment-reply' );
-	// }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'sal_met_scripts' );
 
 /**
  * Implement the Custom Header feature.
  */
-// require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -52,12 +52,12 @@ require get_template_directory() . '/inc/template-tags.php';
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
-// require get_template_directory() . '/inc/template-functions.php';
+require get_template_directory() . '/inc/template-functions.php';
 
 /**
  * Customizer additions.
  */
-// require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
@@ -65,3 +65,19 @@ require get_template_directory() . '/inc/template-tags.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+add_filter( 'nav_menu_css_class', '__return_empty_array', 10, 3 );
